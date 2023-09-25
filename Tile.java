f0d8cca0ed341a70abe29ece676392e5d7bc4fe3
engincanoz <ozengincan424@gmail.com>
2023-09-25 21:23:02 +0300

    matchingfiles method updated

--- Tile.java
+++ Tile.java
@@ -1,6 +1,7 @@
 package OkeyGame;
+
 public class Tile {
-    
+
     int value;
     char color;
 
@@ -8,7 +9,8 @@ public class Tile {
      * Creates a tile using the given color and value, colors are represented
      * using the following letters: Y: Yellow, B: Blue, R: Red, K: Black
      * Values can be in the range [1,13]. There are two tiles of each color value
-     * combination (13 * 2 * 4) = 104 tiles, false jokers are not included in this game.
+     * combination (13 * 2 * 4) = 104 tiles, false jokers are not included in this
+     * game.
      */
     public Tile(int value, char color) {
         this.value = value;
@@ -16,73 +18,60 @@ public class Tile {
     }
 
     /*
-     * TODO: should check if the given tile t and this tile contain the same color and value ---DONE---
+     * TODO: should check if the given tile t and this tile contain the same color
+     * and value ---DONE---
      * return true if they are matching, false otherwise
      */
     public boolean matchingTiles(Tile t) {
-        if( (t.getColor() == this.color) && t.getValue() == this.value) {
+        if ((t.color == this.color) && t.value == this.value) {
             return true;
         }
-        else {
-            return false;
-        }
+        return false;
     }
 
     public int compareToColorFirst(Tile t) {
-        if(colorNameToInt() < t.colorNameToInt()) {
+        if (colorNameToInt() < t.colorNameToInt()) {
             return -1;
-        }
-        else if(colorNameToInt() > t.colorNameToInt()) {
+        } else if (colorNameToInt() > t.colorNameToInt()) {
             return 1;
-        }
-        else{
-             if(getValue() < t.getValue()) {
+        } else {
+            if (getValue() < t.getValue()) {
                 return -1;
-            }
-            else if(getValue() > t.getValue()) {
+            } else if (getValue() > t.getValue()) {
                 return 1;
-            }
-            else{
+            } else {
                 return 0;
             }
         }
     }
 
     public int compareToValueFirst(Tile t) {
-        if(getValue() < t.getValue()) {
+        if (getValue() < t.getValue()) {
             return -1;
-        }
-        else if(getValue() > t.getValue()) {
+        } else if (getValue() > t.getValue()) {
             return 1;
-        }
-        else{
-             if(colorNameToInt() < t.colorNameToInt()) {
+        } else {
+            if (colorNameToInt() < t.colorNameToInt()) {
                 return -1;
-            }
-            else if(colorNameToInt() > t.colorNameToInt()) {
+            } else if (colorNameToInt() > t.colorNameToInt()) {
                 return 1;
-            }
-            else{
+            } else {
                 return 0;
             }
         }
     }
 
     public int colorNameToInt() {
-        if(color == 'Y') {
+        if (color == 'Y') {
             return 0;
-        }
-        else if(color == 'B') {
+        } else if (color == 'B') {
             return 1;
-        }
-        else if(color == 'R') {
+        } else if (color == 'R') {
             return 2;
-        }
-        else {
+        } else {
             return 3;
         }
     }
-    
 
     // determines if this tile can be adjacent to the given tile in a chain
     // returns 0 if the tiles cannot be adjacent in a chain
@@ -90,12 +79,12 @@ public class Tile {
     // returns 2 if they have matching value with different coloring
     public int canFormChainWith(Tile t) {
         // can be adjacent if same color and consecutive number
-        if(t.getColor() == color && Math.abs(t.getValue() - value) == 1) {
+        if (t.getColor() == color && Math.abs(t.getValue() - value) == 1) {
             return 1;
         }
 
         // can be adjacent if same number but different color
-        if(t.getColor() != color && t.getValue() == value) {
+        if (t.getColor() != color && t.getValue() == value) {
             return 2;
         }
 
@@ -113,5 +102,5 @@ public class Tile {
     public char getColor() {
         return color;
     }
-    
+
 }
